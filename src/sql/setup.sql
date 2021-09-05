45d49b48667572b99304feb7227d43d7dacb3c8b
JorgeOsorio97 <jorge.valdez.osorio@gmail.com>
2021-09-05 14:02:41 -0500

    Datawarehouse table creations

---
+++ src/sql/setup.sql
@@ -0,0 +1,93 @@
+-- Dimension country table
+CREATE TABLE IF NOT EXISTS country
+(
+    id   SMALLINT UNSIGNED PRIMARY KEY,
+    name VARCHAR(60)
+) ENGINE = INNODB;
+
+-- Dimension city table
+CREATE TABLE IF NOT EXISTS city
+(
+    id   INT UNSIGNED PRIMARY KEY,
+    name TEXT
+) ENGINE = INNODB;
+
+-- Dimension location table
+CREATE TABLE IF NOT EXISTS location
+(
+    id         INT UNSIGNED PRIMARY KEY,
+    country_id SMALLINT UNSIGNED,
+    city_id    INT UNSIGNED,
+    FOREIGN KEY (country_id) REFERENCES country (id) ON DELETE CASCADE,
+    FOREIGN KEY (city_id) REFERENCES city (id) ON DELETE CASCADE
+) ENGINE = INNODB;
+
+-- Dimension openTo table
+CREATE TABLE IF NOT EXISTS openTo
+(
+    id          SMALLINT UNSIGNED PRIMARY KEY,
+    fulltime    BOOLEAN,
+    flexible    BOOLEAN,
+    internships BOOLEAN,
+    hiring      BOOLEAN,
+    mentors     BOOLEAN,
+    cofounders  BOOLEAN,
+    networking  BOOLEAN
+) ENGINE = INNODB;
+
+-- Facts people table
+CREATE TABLE IF NOT EXISTS people
+(
+    subjectId   INT UNSIGNED PRIMARY KEY,
+    verified    BOOLEAN,
+    weight      DECIMAL(10, 4),
+    location_id INT UNSIGNED,
+    openTo      SMALLINT UNSIGNED,
+    FOREIGN KEY (location_id) REFERENCES location (id) ON DELETE CASCADE,
+    FOREIGN KEY (openTo) REFERENCES openTo (id) ON DELETE CASCADE
+) ENGINE = INNODB;
+
+-- Dimension type table
+CREATE TABLE IF NOT EXISTS type
+(
+    id   SMALLINT UNSIGNED PRIMARY KEY,
+    name VARCHAR(50)
+) ENGINE = INNODB;
+
+-- Dimension status table
+CREATE TABLE IF NOT EXISTS status
+(
+    id     SMALLINT UNSIGNED PRIMARY KEY,
+    status VARCHAR(20)
+) ENGINE = INNODB;
+
+-- Facts opps table
+CREATE TABLE IF NOT EXISTS opps
+(
+    id        INT UNSIGNED PRIMARY KEY,
+    objective VARCHAR(255),
+    remote    BOOLEAN,
+    deadline  DATE,
+    created   DATE,
+    type_id   SMALLINT UNSIGNED,
+    status_id SMALLINT UNSIGNED,
+    FOREIGN KEY (type_id) REFERENCES type (id) ON DELETE CASCADE,
+    FOREIGN KEY (status_id) REFERENCES status (id) ON DELETE CASCADE
+) ENGINE = INNODB;
+
+-- Dimension skill table
+CREATE TABLE IF NOT EXISTS skill
+(
+    id     INT UNSIGNED PRIMARY KEY,
+    name VARCHAR(50)
+) ENGINE = INNODB;
+
+-- Dimension bridge opp_skill table
+CREATE TABLE IF NOT EXISTS opp_skill
+(
+    opp_id INT UNSIGNED,
+    skill_id INT UNSIGNED,
+    PRIMARY KEY (opp_id, skill_id),
+    FOREIGN KEY (opp_id) REFERENCES opps (id) ON DELETE CASCADE,
+    FOREIGN KEY (skill_id) REFERENCES opps (id) ON DELETE CASCADE
+) ENGINE = INNODB;
